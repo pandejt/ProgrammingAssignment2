55e27d325e3fcb86b5ac701539dde6b171a2dcc3
pandejt <pandejt@gmail.com>
2014-10-26 17:00:17 -0500

    Updated Comments to Function

--- cacheSolve.R
+++ cacheSolve.R
@@ -2,7 +2,7 @@
 ## This function must be loaded in the environment ahead of the function
 ## using this one, i.e, makeCacheMatrix
 cacheSolve <- function(x = matrix()) {
-  
+  ## If the inverse is already filled use the existing matrix
   if(!is.null(invM)) {
     message("getting cached data")
     return(invM)
@@ -14,6 +14,10 @@ cacheSolve <- function(x = matrix()) {
     message("DET 0, There is no inverse")  
     return()
   }
+  
+  ## Consideration for inverse filled by a different
+  ## can be checked by comparing the fileld inverse with the x matrix 
+  ## passed to the cacheSolve function
   ## R language function to compute inverse
   invM <<- solve(x)
   invM
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -15,12 +15,15 @@ makeCacheMatrix <- function(x = matrix()) {
   ## Parameter x is internal to makeCacheMatrix environment
   setInvMatrix<-function() {
     ## Call Inverse function
+    ## The called function sets the inverse
     cacheSolve(x)
     }
   
   ## Retrieve inverse matrix
   getInvMatrix<-function() invM
   
+  ## list provide a way to shorten or alter the sub function calls
+  ## s=set allows for a$s() versus a$set()
   list(set=set, get=get,
        setInvMatrix=setInvMatrix,
        getInvMatrix=getInvMatrix)
